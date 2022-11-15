7a6fe44849fbe601d8ab2d8c33d196c6e2a44d06
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-11-15 20:21:09 +0300

    -feat- Added the addTask function now adding tasks are fully functional

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ let formValidation = () => {
     msg.innerHTML = "";
     storageData();
     add.setAttribute("data-bs-dismiss", "modal");
-    add.click()(() => {
+    add.click();
+    (() => {
       add.setAttribute("data-bs-dismiss", "");
     })();
   } else {
@@ -36,4 +37,37 @@ let storageData = () => {
   });
   localStorage.setItem("data", JSON.stringify(data));
   console.log(data);
+  addTask();
+};
+
+let resetForm = () => {
+  textInput.value = "";
+  dateInput.value = "";
+  textArea.value = "";
+};
+
+let addTask = () => {
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+      <div id=${y}>
+        <span class="fw-bold">${x.text}</span>
+        <span class="small text-secondary">${x.date}</span>
+        <p>${x.description}</p>
+        <span class="options">
+          <i
+            onClick="editTask(this)"
+            data-bs-toggle="modal"
+            data-bs-target="#form"
+            class="fas fa-edit"
+          ></i>
+          <i
+            onClick="deleteTask(this);createTasks()"
+            class="fas fa-trash-alt"
+          ></i>
+        </span>
+      </div>
+    `);
+  });
+  resetForm();
 };
