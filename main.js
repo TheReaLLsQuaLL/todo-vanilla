bdfab064c037cb80a3e2bdff2592ddd7d769f495
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-11-15 20:01:27 +0300

    -feat- local storage has been added -bug/fix- Now after adding task the window closes itself

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
 
+let data = [];
+
 let formValidation = () => {
   if (textInput.value !== "") {
     console.log("succes");
     msg.innerHTML = "";
+    storageData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click()(() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
   } else {
     console.log("error");
     msg.innerHTML = "Need a valid input.";
@@ -20,3 +27,13 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   formValidation();
 });
+
+let storageData = () => {
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textArea.value,
+  });
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+};
